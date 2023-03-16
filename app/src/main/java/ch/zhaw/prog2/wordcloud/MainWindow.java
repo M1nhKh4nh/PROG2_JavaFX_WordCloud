9ec45bb407a83bdcac67f686ee620562be8b8490
Minh Khanh Nguyen <nguyemin@students.zhaw.ch>
2023-03-16 18:12:32 +0100

    Implement workshop

---
+++ app/src/main/java/ch/zhaw/prog2/wordcloud/MainWindow.java
@@ -0,0 +1,26 @@
+package ch.zhaw.prog2.wordcloud;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import javafx.fxml.FXMLLoader;
+
+import java.io.IOException;
+
+public class MainWindow extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        openMainWindow(primaryStage);
+    }
+
+    private void openMainWindow(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+        Pane rootNode = loader.load();
+        Scene scene = new Scene(rootNode);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
